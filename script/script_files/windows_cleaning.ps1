14f487f479a6ceda9d7c53c1abeae64a31dcbf15
cesarbrunoms <cesarbrunoms@gmail.com>
2024-01-07 09:44:59 -0400

    update permission

--- script/script_files/windows_cleaning.ps1
+++ script/script_files/windows_cleaning.ps1
@@ -1,3 +1,11 @@
+# Ask for elevated permissions if required
+If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
+	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+	# Enable execution script PowerShell
+	Start-Process powershell.exe "-command set-ExecutionPolicy unrestricted"
+	Exit
+}
+
 
 function windows_cleaning{
     # Remover diretório Windows.old (se existir)
