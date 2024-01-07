85b210cc235124e0cea35160c847f70256f02d7f
cesarbrunoms <cesarbrunoms@gmail.com>
2024-01-07 11:48:58 -0400

    fix clean window

--- script/script_files/dti_pma.ps1
+++ script/script_files/dti_pma.ps1
@@ -21,41 +21,11 @@ DISM /Online /Cleanup-image /Restorehealth
 # defrag
 Optimize-Volume -DriveLetter C -Defrag -TierOptimize -Verbose  
 
-# limpeza CMD
-@echo off
-echo ===============================================================
-echo    Windows Cleanup Script
-echo ===============================================================
-echo.
-
-:: Execute o prompt de comando como administrador
-cd /d "%~dp0"
-if NOT "%1"=="am_admin" (
-    powershell start -verb runas '%0' am_admin
-    goto :eof
-)
-
-:: Inicia a limpeza do disco
-echo Realizando limpeza do disco...
-echo.
-cleanmgr /verylowdisk
-
-:: Remove o diretório Windows.old
-echo Removendo o diretório Windows.old...
-rmdir /s /q C:\Windows.old
-
-:: Limpeza de arquivos de otimização de entrega
-echo Limpando arquivos de otimização de entrega...
-rmdir /s /q "%SystemRoot%\ServiceProfiles\NetworkService\AppData\Local\Microsoft\Windows\DeliveryOptimization\Download"
-rmdir /s /q "%SystemRoot%\ServiceProfiles\NetworkService\AppData\Local\Microsoft\Windows\DeliveryOptimization\Cache"
-
-echo Limpeza concluída com sucesso!
-echo.
-pause
 
 
-# Limpeza powershwll
 
+
+# NOVO
 # Windows Cleanup Script
 
 # Verifica se está sendo executado como administrador
@@ -65,17 +35,26 @@ if (-not $isAdmin) {
     exit
 }
 
-# Limpeza do disco usando Cleanmgr com o parâmetro /LOWDISK
+# Limpeza do disco usando PowerShell
 Write-Host "Realizando limpeza do disco..."
-Start-Process cleanmgr -ArgumentList "/LOWDISK" -Wait
+
+# Função para limpar uma pasta
+function Clear-Folder($path) {
+    if (Test-Path $path) {
+        Get-ChildItem -Path $path | Remove-Item -Force -Recurse -ErrorAction SilentlyContinue
+    }
+}
+
+# Limpeza de arquivos temporários
+Clear-Folder "$env:TEMP"
+
+# Limpeza de arquivos de otimização de entrega
+Clear-Folder "$env:SystemRoot\ServiceProfiles\NetworkService\AppData\Local\Microsoft\Windows\DeliveryOptimization\Download"
+Clear-Folder "$env:SystemRoot\ServiceProfiles\NetworkService\AppData\Local\Microsoft\Windows\DeliveryOptimization\Cache"
 
 # Remoção do diretório Windows.old
 Write-Host "Removendo o diretório Windows.old..."
 Remove-Item -Path "C:\Windows.old" -Recurse -Force -ErrorAction SilentlyContinue
 
-# Limpeza de arquivos de otimização de entrega
-Write-Host "Limpando arquivos de otimização de entrega..."
-Remove-Item -Path "$env:SystemRoot\ServiceProfiles\NetworkService\AppData\Local\Microsoft\Windows\DeliveryOptimization\Download" -Recurse -Force -ErrorAction SilentlyContinue
-Remove-Item -Path "$env:SystemRoot\ServiceProfiles\NetworkService\AppData\Local\Microsoft\Windows\DeliveryOptimization\Cache" -Recurse -Force -ErrorAction SilentlyContinue
-
 Write-Host "Limpeza concluída com sucesso!"
+
